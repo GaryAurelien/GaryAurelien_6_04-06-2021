e82154554869b74c7495d0ec92f94b06d282cb76
Aurélien <aurelien_gary@hotmail.fr>
2021-06-10 14:49:12 +0200

    modification code

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,25 @@
-//on apporte express dans la const express
+/********************Importation modules*********************/
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
-//on cree notre application dans la const app (application express)
+/**************************Sécurité**************************/
+//Helmet vous aide à protéger votre application de certaines des vulnérabilités bien connues du Web en configurant de manière appropriée des en-têtes HTTP.
+const helmet = require('helmet');
+const cors = require('cors');
+
+/**************************Importation route et divers**************************/
+const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user')
+
+
+mongoose.connect('mongodb+srv://Aurelien:Clemence59@cluster0.nqq5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app =  express();
 
 app.use((req, res, next) => {
@@ -11,25 +29,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-    console.log('requete reçue !');
-    next();
-});
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
 
-//methode app.use 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requete a bien été reçue !'});
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès');
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 //on export app que l'on utilisera dans server.js
 module.exports = app;
+
+
