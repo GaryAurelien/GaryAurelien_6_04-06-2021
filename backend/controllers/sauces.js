7cca465c5ac130417e95a67782503940999d1b7f
Aurélien <aurelien_gary@hotmail.fr>
2021-06-17 10:39:39 +0200

    Ajout supp image modif

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -5,27 +5,38 @@ const fs = require('fs');
 /*******************************************Crée sauce****************************************************/
 
 exports.createSauce = (req, res, next) => {
-  const thingObject = JSON.parse(req.body.sauce);
-  delete thingObject._id;
-  const thing = new Sauce({
-    ...thingObject,
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
     .catch(error => res.status(400).json({ error }));
 };
 
 /*******************************************Modification sauce*********************************************/
 
 exports.modifySauce = (req, res, next) => {
-  const thingObject = req.file ?
+  const sauceObject = req.file ?
     {
-      ...JSON.parse(req.body.thing),
+      ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-    Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+  //delete old image if changing image
+  if (req.file !== undefined) {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          console.log("L'image d'origine a bien été supprimée");
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
     .catch(error => res.status(400).json({ error }));
 };
 
@@ -33,11 +44,11 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(thing => {
-      const filename = thing.imageUrl.split('/images/')[1];
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
           .catch(error => res.status(400).json({ error }));
       });
     })
@@ -48,7 +59,7 @@ exports.deleteSauce = (req, res, next) => {
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
+    .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
 };
 
@@ -56,66 +67,67 @@ exports.getOneSauce = (req, res, next) => {
 
 exports.getAllSauce = (req, res, next) => {
   Sauce.find()
-    .then(things => res.status(200).json(things))
+    .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 };
 
 /*******************************************Like Dislike****************************************************/
 
-exports.likeOrDislikeSauce = async(req, res, next)=>{
+exports.likeOrDislikeSauce = async (req, res, next) => {
   try {
     const userId = req.body.userId;
     const likes = req.body.like;
     const id = req.params.id;
     const sauce = await Sauce.findById(id);
-    
+
     switch (likes) { //évalue une expression et, selon le résultat obtenu et le 'cas' associé, exécute les instructions correspondantes.
       case 1: // cas si like=1
         try {
           if (!sauce.usersLiked.includes(userId)) {//vérifier si l'utilisateur est déjà dans le tableau usersLiked
-            await Sauce.findByIdAndUpdate(id, { $inc: {likes: 1}, $push: {usersLiked: userId}}) // incrémenter like et push l'utilisateur dans []usersLiked
-            res.status(201).json({ message: 'vous avez aimé cette sauce !'})    
-        }}catch (error) {
+            await Sauce.findByIdAndUpdate(id, { $inc: { likes: 1 }, $push: { usersLiked: userId } }) // incrémenter like et push l'utilisateur dans []usersLiked
+            res.status(201).json({ message: 'vous avez aimé cette sauce !' })
+          }
+        } catch (error) {
           res.status(400).json({ error })
-        }      
+        }
         break;//L'instruction break permet de terminer la boucle en cours ou l'instruction switch ou label en cours et de passer le contrôle du programme à l'instruction suivant l'instruction terminée.
 
       case 0: // cas si like =0
         try {
-          if (sauce.usersLiked.includes(userId)){ // si l'utilisateur est déjà dans userLiked , décrémentez like et extrayez l'utilisateur du tableau
-            await Sauce.findByIdAndUpdate(id, { $inc: {likes: -1}, $pull: {usersLiked: userId}})
-            res.status(201).json({ message: 'Like retiré !'})
+          if (sauce.usersLiked.includes(userId)) { // si l'utilisateur est déjà dans userLiked , décrémentez like et extrayez l'utilisateur du tableau
+            await Sauce.findByIdAndUpdate(id, { $inc: { likes: -1 }, $pull: { usersLiked: userId } })
+            res.status(201).json({ message: 'Like retiré !' })
             break;
-          } else if (sauce.usersDisliked.includes(userId)){  // si l'utilisateur est déjà dans userDisliked , décrémentez dislike et extrayez l'utilisateur du tableau
-        
-            await Sauce.findByIdAndUpdate(id, { $inc: {dislikes: -1}, $pull: {usersDisliked: userId}})
-            res.status(201).json({ message: 'Dislike retiré !'})
+          } else if (sauce.usersDisliked.includes(userId)) {  // si l'utilisateur est déjà dans userDisliked , décrémentez dislike et extrayez l'utilisateur du tableau
+
+            await Sauce.findByIdAndUpdate(id, { $inc: { dislikes: -1 }, $pull: { usersDisliked: userId } })
+            res.status(201).json({ message: 'Dislike retiré !' })
           }
-          
+
         } catch (error) {
           res.status(400).json({ error })
         }
-        
+
         break;
 
       case -1: //cas si like =-1
         try {
-          if (!sauce.usersDisliked.includes(userId)){ // vérifier si l'utilisateur est déjà dans []usersDisliked
-            await Sauce.findByIdAndUpdate(id, { $inc: {dislikes: 1}, $push: {usersDisliked: userId}}) // incrémente dislike et push l'utilisateur dans []usersDisliked
-            res.status(201).json({ message: "Vous n'avez pas aimé cette sauce !"})
-          
+          if (!sauce.usersDisliked.includes(userId)) { // vérifier si l'utilisateur est déjà dans []usersDisliked
+            await Sauce.findByIdAndUpdate(id, { $inc: { dislikes: 1 }, $push: { usersDisliked: userId } }) // incrémente dislike et push l'utilisateur dans []usersDisliked
+            res.status(201).json({ message: "Vous n'avez pas aimé cette sauce !" })
+
           }
-          
+
         } catch (error) {
-          res.status(400).json({ error })          
-        }       
+          res.status(400).json({ error })
+        }
         break;
 
       default: break;
     }
-  
-  
+
+
   } catch (error) {
-    res.status(400).json({ error });    
+    res.status(400).json({ error });
   };
 };
